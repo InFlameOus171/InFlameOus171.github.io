f9308493ce4799483d11fbbff3fb98f18a00cb30
InFlameOus171 <49519354+InFlameOus171@users.noreply.github.com>
2021-09-14 22:40:55 +0200

    and more changes

--- svg_editor/resources/template.mjs
+++ svg_editor/resources/template.mjs
@@ -1,18 +1,20 @@
-export const main = () => ({
-  tag: "div",
-  id: "drawZone",
-  // inner: [
-  //     {tag: "h4", id: "chatbox-title", inner: '%historytitle%'},
-  //     {
-  //         tag: "div", id: 'chatbox', inner: [
-  //             {id: 'chat-history'},
-  //             {
-  //                 tag: 'div', id: 'input', inner: [
-  //                     {tag: 'input', id: 'input-field'},
-  //                     {tag: 'button', id: 'submit-input', inner: 'Send'}
-  //                 ]
-  //             }
-  //         ]
-  //     }
-  // ]
-});
+export function main() {
+  return {
+    tag: "div",
+    id: "drawZone",
+    // inner: [
+    //     {tag: "h4", id: "chatbox-title", inner: '%historytitle%'},
+    //     {
+    //         tag: "div", id: 'chatbox', inner: [
+    //             {id: 'chat-history'},
+    //             {
+    //                 tag: 'div', id: 'input', inner: [
+    //                     {tag: 'input', id: 'input-field'},
+    //                     {tag: 'button', id: 'submit-input', inner: 'Send'}
+    //                 ]
+    //             }
+    //         ]
+    //     }
+    // ]
+  };
+}
